2c944e4a6eedf1007fb5ff4740caf08d323e75e3
Davide Pedron <davide@pedron.dev>
2025-11-27 14:00:40 +0100

    chore: renaming and reordering function in Main

--- scenes/ui/main_screen/main.gd
+++ scenes/ui/main_screen/main.gd
@@ -11,34 +11,29 @@ func _ready() -> void:
 	Global.game_over.connect(handle_game_over)
 	for client_info in Global.game_state.clients:
 		add_new_client_to_scene(client_info)
-	#$TaskSpawner.start(100)
+
 	if Global.game_state.current_day == 1:
 		$EventSpawner.start(0.2)
 	else:
 		$EventSpawner.start(Config.SECONDS_BETWEEN_EVENTS)
+	$EventSpawner.timeout.connect(handle_spawn_random_event)
 
 	$GameClock.start(Config.DAY_LENGHT_IN_SECONDS)
 	$GameClock.timeout.connect(handle_end_of_the_day)
 
+	$TaskSpawner.start(1)
+	$TaskSpawner.timeout.connect(handle_spawn_tasks)
+
 	get_tree().paused = false
 
-func new_email_from_client() -> void:
-	print("[MAIN] new_email_from_client")
+func handle_spawn_random_event() -> void:
+	print("[MAIN] handle_spawn_random_event")
 	var client_info = ClientData.get_random_client()
 	get_tree().paused = true
 	open_popup_message_for_new_client(client_info)
 
-func _spawn_pending_tasks():
-	print("[MAIN] check {0} clients for spawing tasks".format([Global.game_state.clients.size()]))
-
-func add_new_client_to_scene(client_info: Models.ClientObject) -> void:
-	print("[MAIN] add_new_client_to_scene")
-	var client: ClientScene = client_scene.instantiate()
-	
-	var client_position = Vector2(randi_range(700, screen[0]-110), randi_range(140, screen[1]-160))
-	var translator_position = $Translator.global_position
-	client.initialize(client_position, translator_position, client_info)
-	add_child(client)
+func handle_spawn_tasks():
+	print("[MAIN] handle_spawn_tasks")
 
 func handle_end_of_the_day() -> void:
 	print("[MAIN] handle_end_of_the_day")
@@ -48,6 +43,16 @@ func handle_end_of_the_day() -> void:
 func handle_game_over() -> void:
 	print("[MAIN] handle_game_over")
 	SceneTransition.fade_to_end()
+
+
+func add_new_client_to_scene(client_info: Models.ClientObject) -> void:
+	print("[MAIN] add_new_client_to_scene")
+	var client: ClientScene = client_scene.instantiate()
+	
+	var client_position = Vector2(randi_range(700, screen[0]-110), randi_range(140, screen[1]-160))
+	var translator_position = $Translator.global_position
+	client.initialize(client_position, translator_position, client_info)
+	add_child(client)
 
 func open_popup_message_for_new_client(client: Models.ClientObject) -> void:
 	var popup_data = CustomizablePopupMessage.PopupData.new()
