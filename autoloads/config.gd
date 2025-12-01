64b5a29c412764981635158b571f07149090c91c
Davide Pedron <davide@pedron.dev>
2025-12-01 22:37:35 +0100

    feat: change icon

--- autoloads/config.gd
+++ autoloads/config.gd
@@ -1,8 +1,8 @@
 class_name Config
 extends RefCounted
 
-const DAY_LENGHT_IN_SECONDS: float = 3
-const SECONDS_BETWEEN_EVENTS: int = 10
+const DAY_LENGHT_IN_SECONDS: float = 30
+const SECONDS_BETWEEN_EVENTS: int = 11
 const SECONDS_BETWEEN_TASKS: int = 3
 const WORDS_PER_LETTER: int = 11
 const MAX_STRESS_LEVEL: float = 100
