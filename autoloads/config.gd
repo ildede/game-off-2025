3b17f78738df3bdb58f9ae2de6f35acdb3f0c034
Davide Pedron <davide@pedron.dev>
2026-01-27 13:08:26 +0100

    feat: change initial values

--- autoloads/config.gd
+++ autoloads/config.gd
@@ -1,9 +1,9 @@
 class_name Config
 extends RefCounted
 
-const DAY_LENGHT_IN_SECONDS: float = 30
-const SECONDS_BETWEEN_EVENTS: int = 17
-const SECONDS_BETWEEN_TASKS: int = 3
+const DAY_LENGHT_IN_SECONDS: float = 20
+const SECONDS_BETWEEN_EVENTS: int = 99
+const SECONDS_BETWEEN_TASKS: int = 99
 const WORDS_PER_LETTER: int = 11
 const MAX_STRESS_LEVEL: float = 100
 const MAX_QUALITY_LEVEL: float = 100
@@ -11,4 +11,5 @@ const MAX_REPUTATION_LEVEL: float = 100
 const MAX_CLIENT_LOYALTY: float = 100
 const MIN_CLIENT_LOYALTY: float = 10
 const WORDS_PER_DAY: int = 1000
+const INITIAL_MONEY: int = 2500
 const STRESS_ON_TASK_FAILED: float = 5
